34435e24f4a8288b63fe2bcc4221ee3c3ed64060
delphine <delphin38@live.fr>
2023-11-25 12:15:10 +0100

    scanf avec printf

--- variable/main.c
+++ variable/main.c
@@ -8,10 +8,16 @@ int main(int argc, char *argv[])
     // printf("Hello, World\n");
     // return 0;
 
-    int nombreDeVies = 5, niveau = 1;
-    printf("vous avez %d vies et vous etes au niveau n� %d\n", nombreDeVies, niveau);
-    printf("*** B A M ***\n");
-    nombreDeVies = 4;
-    printf("ah desole, il ne vous reste plus que %d vies maintenant ! \n\n", nombreDeVies);
+    // int nombreDeVies = 5, niveau = 1;
+    // printf("vous avez %d vies et vous etes au niveau n� %d\n", nombreDeVies, niveau);
+    // printf("*** B A M ***\n");
+    // nombreDeVies = 4;
+    // printf("ah desole, il ne vous reste plus que %d vies maintenant ! \n\n", nombreDeVies);
+    // return 0;
+
+    int age = 0; // on initialise
+    printf("Quel age avez-vous ?");
+    scanf("%d", age); // on demande d'entrer l'�ge avec scanf
+    printf("ah ! Vous avez donc %d ans !\n\n", age);
     return 0;
 }
